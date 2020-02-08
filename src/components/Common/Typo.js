2bfd78ab140a7d71e2b8338640952c86aa72f2f4
thanhvk <thanhvk.w3s@gmail.com>
2020-02-09 01:38:39 +0700

    add some common components

---
+++ src/components/Common/Typo.js
@@ -0,0 +1,29 @@
+import { Text } from 'react-native'
+import styled from 'styled-components'
+
+export const P = styled(Text)`
+    color: ${props => props.mute ? '#6e7793' : '#fff'};
+    font-size: 14px;
+    font-family: 'Ubuntu-Regular';
+    margin-bottom: 5px;
+`
+
+export const H1 = styled(Text)`
+    color: #fff;
+    font-size: 18px;
+    font-family: 'Ubuntu-Regular';
+    margin-bottom: 5px;
+`
+
+export const H2 = styled(H1)`
+    font-size: 16px;
+`
+
+export const H3 = styled(H1)`
+    font-size: 14px;
+`
+
+export const Highlight = styled(Text)`
+    color: ${props => props.color ? props.color : '#ff9a4d'};
+`
+
---
+++ src/components/Common/index.js
@@ -0,0 +1,5 @@
+import { P, H1, H2, H3, H4 } from './Typo'
+import { Button } from './Button'
+
+export { P, H1, H2, H3, H4 }
+export { Button }
