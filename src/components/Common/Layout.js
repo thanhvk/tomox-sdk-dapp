0cdf3b60cc420bf7915c48aead22fef0abe4ae1b
thanhvk <thanhvk.w3s@gmail.com>
2020-02-17 17:46:58 +0700

    add common header component

--- src/components/Common/Layout.js
+++ src/components/Common/Layout.js
@@ -1,9 +1,29 @@
+import React from 'react'
 import styled from 'styled-components'
+import { getStatusBarHeight } from 'react-native-status-bar-height'
 import { View } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+import { LgText } from './index'
+
+
+/*
+ * =========================
+ * Container 
+ * =========================
+ */
 
 const Container = styled(View)`
-    flex: 1;
+  flex: 1;
+  background-color: #191e2f;
+  margin-top: ${getStatusBarHeight()}px;
 `
+
+/*
+ * =========================
+ * Row 
+ * =========================
+ */
 
 const Row = styled(View)`
   padding: 10px 0;
@@ -12,5 +32,37 @@ const Row = styled(View)`
   justify-content: space-between;
 ` 
 
-export { Container, Row }
+/*
+ * =========================
+ * Header 
+ * =========================
+ */
+
+function Header({ navigation, title, back }) {
+  return (
+    <HeaderWrapper>
+      <LgText mute>{title}</LgText>
+      {navigation && back && (<BackIcon 
+        name="arrow-left" 
+        onPress={() => navigation.pop()}
+        size={22} />)}
+    </HeaderWrapper>
+  )
+}
+
+const HeaderWrapper = styled(View)`
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  background-color: #252C40;
+`
+
+const BackIcon = styled(Icon)`
+  color: #fff;
+  position: absolute;
+  padding: 12px 10px 10px 10px;
+  left: 0;
+`
+
+export { Container, Row, Header }
 
--- src/components/Common/index.js
+++ src/components/Common/index.js
@@ -1,7 +1,7 @@
 import { P, H1, H2, H3, H4, LgText, MdText } from './Typo'
 import { Button, Input, Select } from './Form'
-import { Container, Row } from './Layout'
+import { Container, Row, Header } from './Layout'
 
 export { P, H1, H2, H3, H4 }
 export { Button, Input, Select, LgText, MdText }
-export { Container, Row }
+export { Container, Row, Header }
